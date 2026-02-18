9e4823755e6141a8a505180c55375eba6d7f24fc
Tristan Li <tristansup5@gmail.com>
2026-02-18 15:37:52 -0500

    Send up prompting

--- routes/ai.py
+++ routes/ai.py
@@ -1,15 +1,21 @@
 from fastapi import APIRouter, status
 from fastapi.responses import JSONResponse
 from google import genai
+from google.genai import types
 
 client = genai.Client()
 ai_router = APIRouter()
 
 
-@ai_router.get('/get/{adviceType}')
-def getAdvice(adviceType: str):
+@ai_router.get('/get/{meal}')
+def getRecipe(meal: str):
     response = client.models.generate_content(
-        model = "gemini-3-flash-preview", contents = "Explain how AI works in a few words"
+        model = "gemini-3-flash-preview", 
+        contents = "Generate the recipe for {meal}",
+        config = types.GenerateContentConfig(
+            system_instruction = "You are a professional restaurant chef for medium income customers",
+            thinking_config = types.ThinkingConfig(thinking_level="low")
+        )
     )
     print(response.text)
-    return JSONResponse(status_code = status.HTTP_200_OK, content = adviceType)
+    return JSONResponse(status_code = status.HTTP_200_OK, content = {'recipe': response.text})
--- routes/setUp.py
+++ routes/setUp.py
@@ -1,16 +1,32 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 class userdata(BaseModel):
+    id: str
     name: str
     username: str
+    sex: bool
     age: int
     height: int
     activityLevel: int
     budget: int
-    dietaryRestrictions: []
+    dietaryRestrictions: [] | None
+    allergies: []
+    dislikes: []
+    cravings: []
     mealsPerDay: int
+    calorieTarget: int
+    sugarTarget: int
+    carbTarget:int
+    proteinTarget: int
+    sleepTarget: int
+    sodiumTarget: int
+    fatTarget: int
+    waterTarget: int
+    pastRecipes: []
+    goals: [] | None
+    units: str
 
 setup_router = APIRouter()
-
 @setup_router.post('/post/{data}')
 def setUp(data: userdata):
     return {"Msg": userdata}
